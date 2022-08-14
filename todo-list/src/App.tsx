1deab65ebdad0c842c9b4c1f8c1ca7fa2deaa6fe
p-acid <qkrtkstjd01@gmail.com>
2022-08-14 13:50:34 +0900

    Modify: CSS to styled-components

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { AppLink, AppLogo, Container, Header } from 'AppStyle';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Container>
+      <Header>
+        <AppLogo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <AppLink href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
-        </a>
-      </header>
-    </div>
+        </AppLink>
+      </Header>
+    </Container>
   );
 }
 
